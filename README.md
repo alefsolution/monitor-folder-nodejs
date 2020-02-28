60d2466adc885571c3642c7d906967538d147538
alefsolutions <alef.solution2020@gmail.com>
2020-02-28 11:05:48 -0600

    upload for showcase feb-28

---
+++ README.md
@@ -0,0 +1,1 @@
+# Node.js monitoring file and folder
---
+++ server.js
@@ -0,0 +1,39 @@
+/**
+ * server.js
+ */
+const ObserveClass = require("./modules/Observe");
+
+// Init Observe object
+let Observe = new ObserveClass();
+
+/**
+ * Watching folder
+ */
+// Define folder to watching, in real project, you should put it in file config or env
+let targetFolder = "./logs";
+
+// Listen event new file has been added
+Observe.on("new-file-has-been-added", (logData) => {
+  // In this step, you can do anything you want, like to push alert message to chatwork, slack...vv
+  // I just print error message to console
+  console.log(logData.message);
+});
+
+// Start watching folder...
+Observe.watchFolder(targetFolder);
+
+/**
+ * Watching file
+ */
+// Define file to watching, in real project, you should put it in file config or env
+let targetFile = "../laravel-example/storage/logs/laravel.log";
+
+// Listen event file has been updated
+Observe.on("file-has-been-updated", (logData) => {
+  // In this step, you can do anything you want, like to push alert message to chatwork, slack...vv
+  // I just print error message to console
+  console.log(logData.message);
+});
+
+// Start watching file...
+Observe.watchFile(targetFile);
